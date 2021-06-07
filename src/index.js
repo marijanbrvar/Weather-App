eb7631f2c6a564ac66d401fb56b4f34c35fbd68b
Marijan Brvar <marijan.brvar@gmail.com>
2021-06-07 12:42:44 +0200

    Add temperature conversion functionality

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import day from './ui/day.svg';
 const app = new AppUi();
 
 const render = function render() {
+  app.createTempToggler();
   app.createLocationForm();
   app.createForecast();
 };
@@ -17,15 +18,23 @@ const card = document.querySelector('.card');
 const forecast = document.querySelector('.condition');
 const cityName = document.querySelector('h5');
 const temp = document.querySelector('#temp');
+const unit = document.querySelector('#unit');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const button = document.querySelector('#converter');
 
 const updateUi = (data) => {
   const { weather, cityDetails } = data;
 
   cityName.innerText = cityDetails.EnglishName;
   forecast.innerText = weather[0].WeatherText;
-  temp.innerText = weather[0].Temperature.Metric.Value;
+  if (button.dataset.temperature === 'C') {
+    temp.innerText = weather[0].Temperature.Metric.Value;
+    unit.innerHTML = '&deg;C';
+  } else {
+    temp.innerText = weather[0].Temperature.Imperial.Value;
+    unit.innerHTML = '&deg;F';
+  }
 
   if (card.classList.contains('d-none')) card.classList.remove('d-none');
 
@@ -33,7 +42,7 @@ const updateUi = (data) => {
   icon.setAttribute('src', iconSrc);
   let timeSrc = null;
 
-  if (weather.isDayTyme) {
+  if (weather[0].IsDayTime) {
     timeSrc = day;
   } else {
     timeSrc = night;
@@ -61,4 +70,24 @@ cityForm.addEventListener('submit', (e) => {
   updateCity(city)
     .then((data) => updateUi(data))
     .catch((err) => console.error(err));
+});
+
+button.addEventListener('click', (e) => {
+  if (e.target.dataset.temperature === 'C') {
+    e.target.dataset.temperature = 'F';
+    e.target.innerHTML = 'Temp. in &deg;F';
+    if (temp.innerText !== '') {
+      const currentVal = parseFloat(temp.innerText, 10).toFixed(1);
+      temp.innerText = ((currentVal * (9 / 5)) + 32).toFixed(1);
+      unit.innerHTML = '&deg;F';
+    }
+  } else {
+    e.target.dataset.temperature = 'C';
+    e.target.innerHTML = 'Temp. in &deg;C';
+    if (temp.innerText !== '') {
+      const currentVal = parseFloat(temp.innerText, 10).toFixed(1);
+      temp.innerText = ((currentVal - 32) * (5 / 9)).toFixed(1);
+      unit.innerHTML = '&deg;C';
+    }
+  }
 });
