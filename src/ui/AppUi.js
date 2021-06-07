ee4ffb799e37e46f847d50ef55e2a951d1614082
Marijan Brvar <marijan.brvar@gmail.com>
2021-06-07 12:41:52 +0200

    Add UI elements for temperature toggle

--- src/ui/AppUi.js
+++ src/ui/AppUi.js
@@ -4,11 +4,15 @@ export default class AppUi {
   constructor() {
     this.container = document.querySelector('.container');
     this.container.classList.add('my-2', 'mx-auto');
-    this.header = document.createElement('h1');
-    this.header.classList.add('text-muted', 'text-center', 'my-4', 'h3');
-    this.header.innerText = 'Weather App';
+    this.header = document.createElement('div');
+    this.header.classList.add('text-center');
+    this.header.id = 'header';
+    this.title = document.createElement('h1');
+    this.title.classList.add('text-muted', 'text-center', 'mt-4', 'mb-2', 'h3');
+    this.title.innerText = 'Weather App';
+    this.header.append(this.title);
     this.location = document.createElement('form');
-    this.location.classList.add('change-location', 'my-4', 'text-center', 'text-muted');
+    this.location.classList.add('change-location', 'mt-2', 'mb-4', 'text-center', 'text-muted');
     this.forecast = document.createElement('div');
     this.forecast.classList.add('card', 'rounded', 'd-none');
     this.container.append(this.header, this.location, this.forecast);
@@ -25,6 +29,16 @@ export default class AppUi {
     this.input.name = 'city';
 
     this.target.append(this.label, this.input);
+  }
+
+  createTempToggler() {
+    this.target = document.querySelector('#header');
+    this.button = document.createElement('button');
+    this.button.id = 'converter';
+    this.button.setAttribute('data-temperature', 'C');
+    this.button.classList.add('btn', 'btn-primary', 'btn-sm');
+    this.button.innerHTML = 'Temp. in &deg;C';
+    this.target.append(this.button);
   }
 
   createForecast() {
@@ -51,10 +65,10 @@ export default class AppUi {
     this.temperature = document.createElement('div');
     this.temperature.classList.add('my-4', 'display-4');
     this.mark = document.createElement('span');
-    this.mark.innerText = 'temp';
+    this.mark.innerText = '';
     this.mark.id = 'temp';
     this.sign = document.createElement('span');
-    this.sign.innerHTML = '&deg;C';
+    this.sign.id = 'unit';
 
     this.temperature.append(this.mark, this.sign);
     this.forecastContent.append(this.icon, this.cityName, this.weatherCondition, this.temperature);
